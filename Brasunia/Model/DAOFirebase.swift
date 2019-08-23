12d1921385724ffb5818edf58f5a70b6478223f2
gabriel <gabrielrgrocha14@gmail.com>
2019-08-23 16:33:43 -0300

    Firebase basics + Model
    FireStore,  FireAuth are installed. Model File, as well as DAOFirebase file, were created. Functions are not used but work.

---
+++ Brasunia/Model/DAOFirebase.swift
@@ -0,0 +1,9 @@
+//
+//  DAOFirebase.swift
+//  Brasunia
+//
+//  Created by Gabriel Gazal on 23/08/19.
+//  Copyright © 2019 Brasunia. All rights reserved.
+//
+
+import Foundation
---
+++ Brasunia/Model/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Brasunia
+//
+//  Created by Gabriel Gazal on 23/08/19.
+//  Copyright © 2019 Brasunia. All rights reserved.
+//
+
+import Foundation
