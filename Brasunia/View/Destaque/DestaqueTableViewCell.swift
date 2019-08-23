19ab04fc0c852e591cdf1f48c2fed70ff4f9a65d
Lary Tertuliano <laryssa.tertuliano@gmail.com>
2019-08-23 16:59:44 -0300

    Tela de destaque em andamento
    falta search bar e xib das células da collection

---
+++ Brasunia/View/Destaque/DestaqueTableViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  HomeTableViewCell.swift
+//  Brasunia
+//
+//  Created by Lary Tertuliano on 23/08/19.
+//  Copyright © 2019 Brasunia. All rights reserved.
+//
+
+import UIKit
+
+class DestaqueTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "DestaqueCell", bundle: nil), forCellWithReuseIdentifier: "cellDestaque")
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
+extension DestaqueTableViewCell: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellDestaque", for: indexPath) as! DestaqueCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width*0.8, height:  collectionView.frame.height)
+    }
+}
---
+++ Brasunia/View/PlanosTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  HomeTableViewCell.swift
+//  Brasunia
+//
+//  Created by Lary Tertuliano on 23/08/19.
+//  Copyright © 2019 Brasunia. All rights reserved.
+//
+
+import UIKit
+
+class PlanosTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellTest")
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
+extension PlanosTableViewCell: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellTest", for: indexPath)
+        cell.backgroundColor = UIColor(white: CGFloat(indexPath.row) * 0.1, alpha: 1)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
