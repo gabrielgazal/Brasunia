1f3116359975e6430dcf832705b833e4f8057203
Kelvin Javorski Soares <kelvin.javorskisoares@gmail.com>
2019-08-29 14:00:09 -0300

    storyboard do projeto e implementação da tableview

---
+++ Brasunia/View/Projeto/ProjetoTitleTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ProjetoTitleTableViewCell.swift
+//  Brasunia
+//
+//  Created by Kelvin Javorski Soares on 28/08/19.
+//  Copyright © 2019 Brasunia. All rights reserved.
+//
+
+import UIKit
+
+class ProjetoTitleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nomeProjetoTxt: UILabel!
+    @IBOutlet weak var imgProjeto: UIImageView!
+    @IBOutlet weak var notaProjetoTxt: UILabel!
+    @IBOutlet weak var labelTxt: UILabel!
+    @IBOutlet weak var infoTxt: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
