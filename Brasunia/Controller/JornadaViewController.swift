73ee0698c46f220af47e106c0dcf843bbf67d92e
Kelvin Javorski Soares <kelvin.javorskisoares@gmail.com>
2019-09-02 15:34:55 -0300

    alterado tela jornada

--- Brasunia/Controller/JornadaViewController.swift
+++ Brasunia/Controller/JornadaViewController.swift
@@ -11,6 +11,15 @@ import UIKit
 class JornadaViewController: UIViewController {
 
     
+    @IBOutlet weak var costuraFullView: UIView!
+    @IBOutlet weak var costuraFillView: UIView!
+    @IBOutlet weak var eletronicaFullView: UIView!
+    @IBOutlet weak var eletronicaFillView: UIView!
+    @IBOutlet weak var marcenariaFullView: UIView!
+    @IBOutlet weak var marcenariaFillView: UIView!
+    
+    
+    
     let Costura = JornadaInfo(trilha: "Costura", icone: "Costura", conquistas: 2, habilidades: 10, x: 100, y: 300, cor: UIColor.red)
     let Marcenaria = JornadaInfo(trilha: "Marcenaria", icone: "Marcenaria", conquistas: 5, habilidades: 14, x: 300, y: 300, cor: UIColor.blue)
     let Eletronica = JornadaInfo(trilha: "Eletronica", icone: "Eletronica", conquistas: 9, habilidades: 10, x: 100, y: 500, cor: UIColor.green)
@@ -18,78 +27,101 @@ class JornadaViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 //        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        createAnimationLayer(Trilha: Costura)
-        createAnimationLayer(Trilha: Marcenaria)
-        createAnimationLayer(Trilha: Eletronica)
+        animaBarraProgresso()
+        
+//        createAnimationLayer(Trilha: Costura)
+//        createAnimationLayer(Trilha: Marcenaria)
+//        createAnimationLayer(Trilha: Eletronica)
     }
     
-    
-    func createAnimationLayer(Trilha trilha: JornadaInfo)
+    func animaBarraProgresso()
     {
-//        let center = view.center
-        //Cria um shapeLayer
-        let shapeLayer = CAShapeLayer()
-        //Cria um track layer
-        let trackLayer = CAShapeLayer()
-        //Cria o caminho do layer
-        let circularPath = UIBezierPath(arcCenter: CGPoint(x: trilha.x, y: trilha.y), radius: 70, startAngle: -CGFloat.pi / 2, endAngle: 1.5 * CGFloat.pi, clockwise: true)
-        
-        //Cria label que dá detalhes sobre as habilidades
-        let percentagemLabel: UILabel = {
-            let label = UILabel()
-            label.text = "\(trilha.trilha)\n\(trilha.conquistas) de \(trilha.habilidades) completas"
-            label.textAlignment = .center
-            label.font = UIFont.systemFont(ofSize: 20)
-            label.numberOfLines = 0
-            return label
-        }()
-        
-        //Definições da track layer
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.lineWidth = 20
-        trackLayer.fillColor = UIColor.clear.cgColor
-        trackLayer.lineCap = .round
-        view.layer.addSublayer(trackLayer)
-        
-        //definições da shape layer
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = trilha.cor.cgColor
-        shapeLayer.lineWidth = 20
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.lineCap = .round
-        shapeLayer.strokeEnd = 0
-        view.layer.addSublayer(shapeLayer)
-        
-        //Insere a label de detalhes no centro dos circulos
-        view.addSubview(percentagemLabel)
-        percentagemLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        percentagemLabel.center = CGPoint(x: trilha.x, y: trilha.y)
-        
-        //Funcao para realizar a animação
-        animateCircle(trilha, shapeLayer)
         
     }
     
-    fileprivate func animateCircle(_ trilha: JornadaInfo, _ shapeLayer: CAShapeLayer) {
-        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        let conquistados = CGFloat(trilha.conquistas)
-        let total = CGFloat(trilha.habilidades)
-        let porcentagem = conquistados / total
+    func criaAnimacao(){
         
-        basicAnimation.toValue = porcentagem
-        basicAnimation.duration = 2
-        
-        //Mantem o resultado após a animação
-        basicAnimation.fillMode = .forwards
-        basicAnimation.isRemovedOnCompletion = false
-        
-        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
     }
+//    fundoView.layer.cornerRadius = 10
+//    completoView.layer.cornerRadius = 10
+//    completoView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+//    
+//    self.completoView.frame.size = CGSize(width: 0, height: self.fundoView.frame.height)
+//    
+//    UIView.animate(withDuration: 2.0, animations: {
+//    self.completoView.frame.size = CGSize(width: self.fundoView.frame.width * progresso, height: self.fundoView.frame.height)
+//    
+//    })
+    
+    
+    
+//    func createAnimationLayer(Trilha trilha: JornadaInfo)
+//    {
+////        let center = view.center
+//        //Cria um shapeLayer
+//        let shapeLayer = CAShapeLayer()
+//        //Cria um track layer
+//        let trackLayer = CAShapeLayer()
+//        //Cria o caminho do layer
+//        let circularPath = UIBezierPath(arcCenter: CGPoint(x: trilha.x, y: trilha.y), radius: 70, startAngle: -CGFloat.pi / 2, endAngle: 1.5 * CGFloat.pi, clockwise: true)
+//
+//        //Cria label que dá detalhes sobre as habilidades
+//        let percentagemLabel: UILabel = {
+//            let label = UILabel()
+//            label.text = "\(trilha.trilha)\n\(trilha.conquistas) de \(trilha.habilidades) completas"
+//            label.textAlignment = .center
+//            label.font = UIFont.systemFont(ofSize: 20)
+//            label.numberOfLines = 0
+//            return label
+//        }()
+//
+//        //Definições da track layer
+//        trackLayer.path = circularPath.cgPath
+//        trackLayer.strokeColor = UIColor.lightGray.cgColor
+//        trackLayer.lineWidth = 20
+//        trackLayer.fillColor = UIColor.clear.cgColor
+//        trackLayer.lineCap = .round
+//        view.layer.addSublayer(trackLayer)
+//
+//        //definições da shape layer
+//        shapeLayer.path = circularPath.cgPath
+//        shapeLayer.strokeColor = trilha.cor.cgColor
+//        shapeLayer.lineWidth = 20
+//        shapeLayer.fillColor = UIColor.clear.cgColor
+//        shapeLayer.lineCap = .round
+//        shapeLayer.strokeEnd = 0
+//        view.layer.addSublayer(shapeLayer)
+//
+//        //Insere a label de detalhes no centro dos circulos
+//        view.addSubview(percentagemLabel)
+//        percentagemLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//        percentagemLabel.center = CGPoint(x: trilha.x, y: trilha.y)
+//
+//        //Funcao para realizar a animação
+//        animateCircle(trilha, shapeLayer)
+//
+//    }
+//
+//    fileprivate func animateCircle(_ trilha: JornadaInfo, _ shapeLayer: CAShapeLayer) {
+//        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+//        let conquistados = CGFloat(trilha.conquistas)
+//        let total = CGFloat(trilha.habilidades)
+//        let porcentagem = conquistados / total
+//
+//        basicAnimation.toValue = porcentagem
+//        basicAnimation.duration = 2
+//
+//        //Mantem o resultado após a animação
+//        basicAnimation.fillMode = .forwards
+//        basicAnimation.isRemovedOnCompletion = false
+//
+//        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+//    }
 }
 
 
